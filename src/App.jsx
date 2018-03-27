11a92520818ed8f13b5c9f719ec4dd9f9bc08264
dave <dsersch640@gmail.com>
2018-03-26 18:14:20 -0700

    lesson 33

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    // return (
-      // <div className="App">
-      //   <h1>Hi, I'm a React app</h1>
-      // </div>
-      // );
-      return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Is this working???'))
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <p>This is really working...</p>
+        <Person />
+      </div>
+      );
   }
 }
 
