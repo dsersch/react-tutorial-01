5923291144f71408c3be242011f4d7287c81cf72
dave <dsersch640@gmail.com>
2018-03-28 15:22:18 -0700

    lesson 66

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
 import './App.css';
 import Person from "./Person/Person";
 
@@ -48,10 +47,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     }
 
     let persons = null;
@@ -72,10 +67,6 @@ class App extends Component {
           })}
       </div>
       style.backgroundColor = "red";
-      style[':hover'] = {
-        backgroundColor: 'tomato',
-        color: 'black'
-      }
     }
 
     let classes = [];
@@ -101,4 +92,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
